32c452f8a2a436ccf60ddb20f22d943a0f6184d7
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-23 13:59:16 +0900

    テストのためのgemをinstallした

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,35 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :date_of_birth, presence: true
+  validate :password_complexity
+  validate :validate_full_width_characters
+
+  has_many :products
+  has_many :orders
+
+  private
+
+  def password_complexity
+    return if password.blank? || (password =~ /(?=.*[a-zA-Z])(?=.*\d)/)
+
+    errors.add :password, 'must include both letters and numbers'
+  end
+
+  def validate_full_width_characters
+    validate_full_width(last_name, 'last_name')
+    validate_full_width(first_name, 'first_name')
+    validate_full_width(last_name_kana, 'last_name_kana', /\A[ァ-ヶー－]+\z/)
+    validate_full_width(first_name_kana, 'first_name_kana', /\A[ァ-ヶー－]+\z/)
+  end
+
+  def validate_full_width(attribute, field_name, regex = /\A[ぁ-んァ-ン一-龥々〆〤]+\z/)
+    value = send(attribute)
+    puts "Validating #{attribute}: #{value}" # デバッグ用出力
+    return if value.blank?
+
+    return if value =~ regex
+
+    puts "Validation failed for #{attribute}" # デバッグ用出力
+    errors.add field_name.to_sym, 'must be in full-width characters'
+  end
 end
