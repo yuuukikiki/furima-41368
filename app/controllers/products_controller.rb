0e1ecaf880f3aac55526adbc5cf07467d10c7905
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-26 12:44:02 +0900

    出品ページのコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,23 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = current_user.products.build(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :condition_id,
+                                    :shipping_fee_id, :prefecture_id, :shipping_time_id, :price, :image)
   end
 end
