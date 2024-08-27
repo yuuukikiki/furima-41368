6013682e8428ba6e6fbdf806f62d86e9e416aeeb
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-27 12:00:34 +0900

    商品一覧表示のコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
+    @products = Product.all.order(created_at: :desc)
   end
 
   def new
