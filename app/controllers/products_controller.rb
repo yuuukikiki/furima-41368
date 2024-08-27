4557d8468b3ac1bc7812d63732acbb7fcc786e06
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-27 18:42:13 +0900

    商品編集ページのコントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_product, only: :show
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -17,8 +18,19 @@ class ProductsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -26,6 +38,10 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user == @product.user
   end
 
   def product_params
