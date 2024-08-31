87b94fca2a15a75d3d45bcf7513d9c6132957cda
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-31 14:13:13 +0900

    OrdersControllerの訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,7 +27,7 @@ class OrdersController < ApplicationController
   end
 
   def check_access
-    return unless @product.is_sold_out? || (current_user && current_user != @product.user)
+    return unless @product.is_sold_out? || (current_user && (current_user == @product.user || @product.is_sold_out?))
 
     redirect_to root_path
   end
